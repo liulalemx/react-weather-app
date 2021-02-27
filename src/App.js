fe636140c9155ed16e76d4c928349899f1affdc2
liul <48413031+liulaz@users.noreply.github.com>
2021-02-27 21:24:57 +0300

    Test Weather API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import { useEffect } from "react";
+
 function App() {
+  const auth = "4830fbc71898dbf53713b1e4fff60c6a";
+  let searchValue = "Addis Ababa";
+  let img;
+  // const log = "04n";
+
+  useEffect(() => {
+    const getWeather = async () => {
+      const weatherFromServer = await fetchWeather();
+    };
+
+    getWeather();
+  });
+
+  // Fetch Data
+  const fetchWeather = async () => {
+    const result = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=${auth}`
+    );
+    const data = await result.json();
+    return data;
+  };
+
   return (
     <div className="App">
       <h1>Weather App</h1>
+
+      <img src={`http://openweathermap.org/img/wn/04n@2x.png`} alt="" />
     </div>
   );
 }
